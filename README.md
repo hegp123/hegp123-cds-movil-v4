7ed3a94e22e8fd85121ad8fb94c1f82bf34f4dc6
Hector Garcia <hectoregarciap@hotmail.com>
2019-05-24 07:52:27 -0500

    plantilla de tabs

---
+++ README.md
@@ -0,0 +1,4 @@
+ionic serve
+ionic cordova platform rm android
+ionic cordova platform add android
+ionic cordova run android
--- src/app/pages/shared/footer/footer.component.ts
+++ src/app/pages/shared/footer/footer.component.ts
@@ -1,14 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss'],
+  selector: "app-footer",
+  templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.scss"]
 })
 export class FooterComponent implements OnInit {
+  @Input() type: string = "payment";
 
-  constructor() { }
+  payment: string = "assets/img/registrarPago.png";
+  print: string = "assets/img/reimpFactura.png";
+  report: string = "assets/img/reportePagos.png";
+  paymentAct: string = "assets/img/registrarPagoAct.png";
+  printAct: string = "assets/img/reimpFacturaAct.png";
+  reportAct: string = "assets/img/reportePagosAct.png";
 
-  ngOnInit() {}
+  paymentImg: string;
+  printImg: string;
+  reportImg: string;
 
+  paymentText: string;
+  printText: string;
+  reportText: string;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.paymentImg = this.type === "payment" ? this.paymentAct : this.payment;
+    this.printImg = this.type === "print" ? this.printAct : this.print;
+    this.reportImg = this.type === "report" ? this.reportAct : this.report;
+
+    this.paymentText = this.type === "payment" ? "textEnabled" : "textDisabled";
+    this.printText = this.type === "print" ? "textEnabled" : "textDisabled";
+    this.reportText = this.type === "report" ? "textEnabled" : "textDisabled";
+  }
 }
