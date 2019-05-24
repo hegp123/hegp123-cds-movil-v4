b1a8195cd5d818bf942e4ce435694f81fc5916c2
Hector Garcia <hectoregarciap@hotmail.com>
2019-05-24 08:20:59 -0500

    plantilla de tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 const routes: Routes = [
   { path: "", redirectTo: "payment", pathMatch: "full" },
   { path: "login", loadChildren: "./pages/login/login.module#LoginPageModule" },
-  { path: "tabs", loadChildren: "./pages/tabs/tabs.module#TabsPageModule" },
   { path: "payment", loadChildren: "./pages/tabs/payment/payment.module#PaymentPageModule" }
 ];
 
